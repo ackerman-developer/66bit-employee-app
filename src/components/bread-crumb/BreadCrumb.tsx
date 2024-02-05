dc21ee1e0b49f86c3f62280e8463fb14c7b4cb32
Artur Imankulov <arturimankulov906@gmail.com>
2024-02-05 07:27:27 +0500

    create bread-crumb

--- src/components/bread-crumb/BreadCrumb.tsx
+++ src/components/bread-crumb/BreadCrumb.tsx
@@ -1,38 +1,27 @@
-// import { Link, useLocation } from 'react-router-dom'
+import { Link, useParams, useLocation } from 'react-router-dom'
 import styles from './BreadCrumb.module.scss'
-import Crumb from './crumb/Crumb'
-// import Arrow from '../../assets/images/arrow.svg?react'
-// import useEmployeeById from '../../hooks/employee-by-id'
-// import { AppRoutes } from '../../const'
+import Arrow from '../../assets/images/arrow.svg?react'
+import useEmployeeById from '../../hooks/useEmployeeById'
+import { AppRoutes } from '../../const'
 
 export default function BreadCrumb() {
-  // const { pathname } = useLocation()
-  // const employee = useEmployeeById()
-  // const pathnames = pathname.split('/').filter((x) => x)
-  // let breadcrumbPath = ''
+  const { id } = useParams()
+  const location = useLocation()
+  const employee = useEmployeeById()
 
-  // const getTitle = () => {
-  //   switch (location.pathname) {
-  //     case AppRoutes.listEmployees:
-  //       return "Список сотрудников"
-  //     case `${AppRoutes.aboutEmployee}/${employee.id}`:
-  //       return `${employee.name}`
-  //   }
-  // }
+  const isEmployeeDetailsPage = location.pathname.startsWith(`${AppRoutes.aboutEmployee}/${id}`)
 
-  return(
+  return (
     <div className={styles.component}>
-      <Crumb />
-      {/* {pathnames.map((name, index) => {
-        breadcrumbPath +=`${name}`
-        const isLast = index===pathnames.length-1
-        return isLast? <span>{name}</span>:(
-          <>
-            <Arrow />
-            <Link to={breadcrumbPath}>{name}</Link>
-          </>
-        )
-      })} */}
+      <Link to='/'>Главная</Link>
+      <Arrow />
+      <Link to='/'>Список сотрудников</Link>
+      {isEmployeeDetailsPage && employee &&
+        <>
+          <Arrow />
+          <p>{employee.name}</p>
+        </>
+      }
     </div>
   )
 }
