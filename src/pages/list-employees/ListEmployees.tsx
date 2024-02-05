5a274da14cfdba7a742be4503c0c81a718021663
Artur Imankulov <arturimankulov906@gmail.com>
2024-02-05 17:24:54 +0500

    create mobile version

--- src/pages/list-employees/ListEmployees.tsx
+++ src/pages/list-employees/ListEmployees.tsx
@@ -1,9 +1,11 @@
 import TableEmpoyees from "../../components/table-employees/TableEmployees";
+import styles from './ListEmployees.module.scss'
 
 export default function ListEmployees() {
   return(
-    <>
+    <div className={styles.container}>
+      <h1>Список сотрудников</h1>
       <TableEmpoyees />
-    </>
+    </div>
   )
 }
--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,15 +1,21 @@
 import { ReactNode, useState } from "react";
 import { Theme, ThemeContext } from "../context/ThemeContext";
-import { changeCssRootVariables } from "../model/ChangeCssRootVariables";
+import { changeCssRootVariables } from "../model/changeCssRootVariables";
+import { storage } from "../model/storage";
 
 interface IProvider {
   children: ReactNode
 }
 
 export default function ThemeProvider({children, ...props}: IProvider) {
-  const [ theme, setTheme ] = useState<Theme>(Theme.LIGHT)
+  const [ theme, setTheme ] = useState<Theme>(
+    storage.getItem('theme') || Theme.LIGHT
+  )
+
+  changeCssRootVariables(theme)
 
   const changeTheme = (theme: Theme) => {
+    storage.setItem('theme', theme)
     setTheme(theme)
     changeCssRootVariables(theme)
   }
